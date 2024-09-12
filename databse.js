ca0c0e6c95264bc17436840c7c091c043c47e5b1
Deepak-sharma2003 <deepak.223008@gmail.com>
2024-09-12 13:09:27 +0530

    Update databse.js

--- databse.js
+++ databse.js
@@ -1,21 +1,47 @@
-const sql=require("mysql2");
+// const sql=require("mysql2");
 
-const con=sql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"mydb"
-})
+// const con=sql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"mydb"
+// })
 
-async function databaseconnection() {
-    con.connect((err,data)=>{
-        if(err) return console.log("Err : ",err);
+// async function databaseconnection() {
+//     con.connect((err,data)=>{
+//         if(err) return console.log("Err : ",err);
     
+//         console.log("Database Connected!!");
+//     });
+// }
+
+// module.exports = {
+//     databaseconnection,
+//     con
+// };
+const mysql = require("mysql2/promise");
+
+const con = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb",
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+async function databaseConnection() {
+    try {
+        // Test the connection
+        const [rows, fields] = await con.query('SELECT 1');
         console.log("Database Connected!!");
-    });
+    } catch (err) {
+        console.error("Error connecting to the database:", err.message);
+    }
 }
 
 module.exports = {
-    databaseconnection,
+    databaseConnection,
     con
 };
